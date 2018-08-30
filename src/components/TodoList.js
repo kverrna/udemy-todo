e4d574fe2ea34addae4c8c283dc48961764a0b5c
Ramon cruz <ramon.silva@bbtecno.com.br>
2018-08-30 17:00:34 -0300

    touch no item da lista

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import { connect } from 'react-redux';
 
 const TodoList = ({ todos }) => (
   <View style={{ margin: 1, padding: 10 }}>
-    {todos.map(todo => <Text key={todo.id} style={{ padding: 10, backgroundColor: '#ff1', margin: 2 }}>{todo.text}</Text>)}
+
+    {todos.map(todo => (
+      <TouchableOpacity onPress={() => {}}>
+        <Text key={todo.id} style={{ padding: 10, backgroundColor: '#ff1', margin: 2 }}>
+          {todo.text}
+        </Text>
+      </TouchableOpacity>
+    ))}
   </View>
 );
 const mapStateToProps = (state) => {
